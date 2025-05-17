780348fea83ee2b737d3f4153987e110d4c19c2a
Shagor-Mia <infinityshagor18557@gmail.com>
2025-05-17 21:06:50 +0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // Add Morgan middleware
+  app.use(morgan('dev')); // or 'combined', 'tiny', etc.
+
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log('server running on port 3000.');
+  });
+}
+bootstrap().catch((err) => {
+  console.error('Failed to start the app', err);
+  process.exit(1); // Optional: Exit with failure code
+});
